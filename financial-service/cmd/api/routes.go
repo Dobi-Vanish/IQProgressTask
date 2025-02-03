545738e7d119c73f6359b475db0ce6d77d87e8f9
Dobi-Vanish <ilopi567@mail.ru>
2025-02-03 22:40:32 +0300

    set routes

--- financial-service/cmd/api/routes.go
+++ financial-service/cmd/api/routes.go
@@ -5,7 +5,6 @@ import (
 )
 
 func (app *Config) routes(router *gin.Engine) {
-	// Настройка CORS
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
@@ -22,14 +21,12 @@ func (app *Config) routes(router *gin.Engine) {
 		c.Next()
 	})
 
-	// Middleware для проверки работоспособности
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	// Маршруты
 	router.POST("/depositMoney", app.depositMoney)
 	router.POST("/transferMoney", app.transferMoney)
 	router.GET("/getLastTransactions", app.GetLastTransactions)
